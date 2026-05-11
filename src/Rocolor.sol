5e714071bf398f9dbcb244c802aef9221ffe5a1e
m3 <mlamont@gmail.com>
2026-05-11 07:18:06 -0400

    deploy'd locally via keystore

--- src/Rocolor.sol
+++ src/Rocolor.sol
@@ -1,9 +1,20 @@
 // SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.33;
+pragma solidity 0.8.33;
 
 contract Rocolor {
     // this is it!
+
+    /// @dev Custom string per unique tokenId, which can appear in the NFT pic.
+    // mapping(uint => string) private _names;
+    string private _name;
+
+    function setName(string memory name) public {
+        _name = name;
+    }
+
+    function getName() public view returns (string memory) {
+        return _name;
+    }
 
     constructor() {
         // starting stuff
