3d6b904135680ae92fc7234441d27162398c7813
m3 <mlamont@gmail.com>
2026-05-26 18:49:13 -0400

    outfunding testing++

--- test/RocolorTestOutfunding.t.sol
+++ test/RocolorTestOutfunding.t.sol
@@ -46,13 +46,56 @@ contract RocolorTestOutfunding is Test, Rocolor, RocolorTestHelpers {
 
         assertEq(address(msg.sender).balance, senderInitialBalance + sentAmount);
     }
+
+    function testWithdraw_Emits() public {
+        uint256 minterInitialBalance = address(rocolor).balance;
+        uint256 sendAmount = 1 ether;
+
+        vm.prank(HERO);
+        rocolor.mintColor{value: sendAmount}(MURPH_LIGHT_HEX_TRIPLET, MURPH_LIGHT_COLOR_NAME);
+
+        vm.prank(address(msg.sender));
+        vm.expectEmit(true, false, false, false);
+        emit ROColor__ContractBalanceWithdrawalPassed(minterInitialBalance + sendAmount);
+        rocolor.withdraw();
+    }
+
+    function testWithdraw_HappyLongPath() public {
+        uint256 minterInitialBalance = address(rocolor).balance;
+        uint256 receiverInitialBalance = address(msg.sender).balance;
+        uint256 sendAmount = 1 ether;
+
+        // 1st mint
+        vm.prank(HERO);
+        rocolor.mintColor{value: sendAmount}(MURPH_LIGHT_HEX_TRIPLET, MURPH_LIGHT_COLOR_NAME);
+        // minter contract total funds: 1 ether
+
+        // 2nd mint
+        vm.deal(FRIEND, 20 ether);
+        vm.prank(FRIEND);
+        rocolor.mintColor{value: sendAmount}("000001", "Darkest Blue");
+        // minter contract total funds: 2 ether
+
+        // withdraw
+        vm.prank(address(msg.sender));
+        vm.expectEmit(true, false, false, false);
+        emit ROColor__ContractBalanceWithdrawalPassed(minterInitialBalance + sendAmount + sendAmount);
+        rocolor.withdraw();
+        assertEq(address(rocolor).balance, 0);
+        assertEq(address(msg.sender).balance, receiverInitialBalance + minterInitialBalance + sendAmount + sendAmount);
+
+        // 3rd mint
+        vm.prank(HERO);
+        rocolor.mintColor{value: sendAmount}("000002", "Almost Darkest Blue");
+        assertEq(address(rocolor).balance, sendAmount);
+    }
 }
 // backlog
 // / happy path
-// Emits a ROColor__ContractBalanceWithdrawalPassed event
-// happy long path:
-// ... withdraw correct amount after 2 mints
-// ... can mint after withdraw (to see balance goes up after it goes to 0)
+// / Emits a ROColor__ContractBalanceWithdrawalPassed event
+// / happy long path:
+// / ... withdraw correct amount after 2 mints
+// / ... can mint after withdraw (to see balance goes up after it goes to 0)
 // Reverts if contract is owned by someone else
 // Reverts if contract has no funds to withdraw
 // Reverts if fund withdrawal failed (2 ways)
